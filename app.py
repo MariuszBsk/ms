511d6ccc2a60666deb5290fe28b9a9cbcca0734b
Mariusz <aroundus281@gmail.com>
2025-02-13 16:57:23 +0000

    app.py

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def generate_image():
     return jsonify({"message": "Feature not implemented yet"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
 
